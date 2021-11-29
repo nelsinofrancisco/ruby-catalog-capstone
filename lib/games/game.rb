5094af8add0392d15870e4df8d19b5d8d35e4260
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-11-29 14:17:28 -0500

    Create Game and Author classes

---
+++ lib/games/game.rb
@@ -0,0 +1,16 @@
+relative_require './../item'
+
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+  attr_reader :archived
+
+  def initialize(multiplayer, last_played_at, published_date, archived: false)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+    @super(published_date, archived: false)
+  end
+
+  def can_be_archived?
+    false unless self.can_be_archived? === true && self.last_played_at < 2
+  end
+end
---
+++ lib/items/author.rb
@@ -0,0 +1,15 @@
+class Author
+  attr_accessor :first_name, :last_name, :items
+  attr_reader :id
+
+  def initialize(first_name, last_name)
+    @id = Random.rand(1...1000)
+    @first_name = first_name
+    @last_name= last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+  end
+end
