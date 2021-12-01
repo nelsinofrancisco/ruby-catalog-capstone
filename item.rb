2d69d533dcf902ee0f6716aa551e0dbc0df42f3d
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-12-01 16:41:18 -0500

    Add author class unit testing and finish game class unit testing

--- item.rb
+++ item.rb
@@ -12,7 +12,7 @@ class Item
 
   def add_author(author)
     @author = author
-    author.add_item(self)
+    author.add_item(self) unless author.items.include?(self)
   end
 
   def add_genre(genre)
--- lib/games/spec/game_spec.rb
+++ lib/games/spec/game_spec.rb
@@ -1,31 +1,30 @@
 require_relative '../game.rb'
 
 describe Game do
-  context "When testing the Game class" do 
-    it "Should create a new game when we call the class new method" do 
-      new_game = Game.new('test', true, 3, 5)
-      expect(new_game.title).to eq "test"
-      expect(new_game.multiplayer).to eq true
-      expect(new_game.last_played_at).to eq 3
-      expect(new_game.published_date).to eq 5
+  describe 'Testing the Game class' do
+    context "When creating new Game class" do 
+      it "Should create a new game when we call the class new method" do 
+        new_game = Game.new('test', true, 3, 5)
+        expect(new_game.title).to eq "test"
+        expect(new_game.multiplayer).to eq true
+        expect(new_game.last_played_at).to eq 3
+        expect(new_game.published_date).to eq 5
+      end
     end
   end
 
-  # context "When testing the game class" do 
-  #   it "Should answer with true or false if the game can use services calling can_use_services? method" do 
-  #     new_game = Game.new('1', 30, 'robert')
-  #     expect(new_game.can_use_services?).to eq true
-  #     new_game1 = Game.new('2', 15, 'jules', false)
-  #     expect(new_game1.can_use_services?).to eq false
-  #   end
-  # end
+  describe 'Testing the Game class methods' do
+    context "When can_be_archived method is called" do 
+      it "Should return true if parent's method returns true AND if last_played_at is older than 2 years, otherwise returns false" do 
+        new_game1 = Game.new('test', true, 3, 5)
+        expect(new_game1.can_be_archived?).to eq false
 
-  # context "When testing the game class" do 
-  #   it "Should correct the game name capitalizing first letter and cut to 10 characters with validate_name method" do 
-  #     new_game = game.new('1', 30, 'robert')
-  #     expect(new_game.validate_name).to eq 'Robert'
-  #     new_game1 = game.new('2', 15, 'julesofthesaintroad')
-  #     expect(new_game1.validate_name).to eq 'Julesofthe'
-  #   end
-  # end
+        new_game2 = Game.new('test', true, 4, 11)
+        expect(new_game2.can_be_archived?).to eq true
+
+        new_game3 = Game.new('test', true, 1, 15)
+        expect(new_game3.can_be_archived?).to eq false
+      end
+    end
+  end
 end
