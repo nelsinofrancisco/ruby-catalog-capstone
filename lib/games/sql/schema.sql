0219bbadb46f39da9fc0bb76401123a9bef5f370
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-12-01 13:31:37 -0500

    Add schema.sql with all interactions between tables

--- lib/games/sql/schema.sql
+++ lib/games/sql/schema.sql
@@ -1,3 +1,19 @@
+
+CREATE TABLE games (
+  id int GENERATED ALWAYS AS IDENTITY(MINVALUE 0 START WITH 0 CACHE 20) NOT NULL,
+  PRIMARY KEY (id),
+  title varchar(30),
+  multiplayer boolean,
+  last_played_at date
+);
+
+CREATE TABLE authors (
+  id int GENERATED ALWAYS AS IDENTITY(MINVALUE 0 START WITH 0 CACHE 20) NOT NULL,
+  PRIMARY KEY (id),
+  first_name varchar(30),
+  last_name varchar(30)
+);
+
 CREATE TABLE items (
   id int GENERATED ALWAYS AS IDENTITY(MINVALUE 0 START WITH 0 CACHE 20) NOT NULL,
   PRIMARY KEY (id),
@@ -6,20 +22,9 @@ CREATE TABLE items (
   source varchar(30),
   label varchar(30),
   publish_date int,
-  archived boolean
-)
-
-CREATE TABLE games (
-  id int GENERATED ALWAYS AS IDENTITY(MINVALUE 0 START WITH 0 CACHE 20) NOT NULL,
-  PRIMARY KEY (id),
-  title varchar(30),
-  multiplayer boolean,
-  last_played_at date
-)
-
-CREATE TABLE authors (
-  id int GENERATED ALWAYS AS IDENTITY(MINVALUE 0 START WITH 0 CACHE 20) NOT NULL,
-  PRIMARY KEY (id),
-  first_name varchar(30),
-  last_name varchar(30)
-)
+  archived boolean,
+  authors_id int NOT NULL,
+  games_id int NOT NULL,
+  CONSTRAINT kf_authors FOREIGN KEY(authors_id) REFERENCES authors(id) ON DELETE CASCADE,
+  CONSTRAINT kf_games FOREIGN KEY(games_id) REFERENCES games(id) ON DELETE CASCADE
+);
