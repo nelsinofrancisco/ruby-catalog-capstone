b8e149fcfa6d73bd587c48523f08f131c4c425f4
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-01 17:02:44 -0300

    chore: add game.title to the function that display the list of items to be added in the 1-* relational classes

--- lib/helpers/storage_handler.rb
+++ lib/helpers/storage_handler.rb
@@ -60,7 +60,7 @@ module StorageHandler
       general_item + book_chr
     when 'Game'
       general_item = "[#{index}] - Class: #{item.class} Id: #{item.id}, Published at: #{item.published_date} "
-      game_chr = "Characteristics: #{item.multiplayer}, #{item.last_played_at}"
+      game_chr = "Characteristics: #{item.title} #{item.multiplayer}, #{item.last_played_at}"
       general_item + game_chr
     when 'MusicAlbum'
       general_item = "[#{index}] - Class: #{item.class} Id: #{item.id}, Published at: #{item.published_date} "
