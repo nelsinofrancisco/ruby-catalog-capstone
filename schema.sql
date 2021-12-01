2d645330fde73faaec9d4f4fda4056df3d4bc6ef
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-12-01 18:56:26 -0500

    Add changes required by CR

--- schema.sql
+++ schema.sql
@@ -40,7 +40,11 @@ CREATE TABLE items (
   archived boolean,
   author_id int,
   game_id int,
+  album_id int, 
+  genre_id int,
   CONSTRAINT kf_authors FOREIGN KEY(author_id) REFERENCES authors(id) ON DELETE CASCADE,
-  CONSTRAINT kf_games FOREIGN KEY(game_id) REFERENCES games(id) ON DELETE CASCADE
+  CONSTRAINT kf_games FOREIGN KEY(game_id) REFERENCES games(id) ON DELETE CASCADE,
+  CONSTRAINT kf_albums FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE,
+  CONSTRAINT kf_genres FOREIGN KEY(genre_id) REFERENCES genres(id) ON DELETE CASCADE
 ); 
 
